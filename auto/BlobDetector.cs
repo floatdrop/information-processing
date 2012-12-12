68de0026b7f40fe62cd4499e65d8f096e3498585
Vsevolod Strukchinsky <vstrukchinsky@skbkontur.ru>
2012-12-12 15:50:40 +0600

    added some tracking functionality

--- auto/BlobDetector.cs
+++ auto/BlobDetector.cs
@@ -1,26 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.VideoSurveillance;
 
 namespace auto
 {
 	static class BlobDetector
 	{
+		private static MCvFont _font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_SIMPLEX, 1.0, 1.0);
+		private static FGDetector<Bgr> _detector = new FGDetector<Bgr>(FORGROUND_DETECTOR_TYPE.FGD);
+		private static BlobTrackerAuto<Bgr> _tracker = new BlobTrackerAuto<Bgr>();
+
+		private static void TrackBlobs(Image<Bgr, Byte> frame)
+		{
+			frame._SmoothGaussian(3); //filter out noises
+			_detector.Update(frame);
+			Image<Gray, Byte> forgroundMask = _detector.ForgroundMask;
+			_tracker.Process(frame, forgroundMask);
+			foreach (MCvBlob blob in _tracker)
+			{
+				frame.Draw(Rectangle.Round(blob), new Bgr(255.0, 255.0, 255.0), 2);
+				frame.Draw(blob.ID.ToString(), ref _font, Point.Round(blob.Center), new Bgr(255.0, 255.0, 255.0));
+			}
+		}
+
+		private static void FloodFill(Image<Bgr, byte> image)
+		{
+			for (int i = 0; i < image.Width; i++)
+			{
+				for (int j = 0; j < image.Height; j++)
+				{
+					if (image.Data[j, i, 0] != 255)
+					{
+						Image<Bgr, byte> image_copy = image.Copy();
+						Image<Gray, byte> mask = new Image<Gray, byte>(image.Width + 2, image.Height + 2);
+						MCvConnectedComp comp = new MCvConnectedComp();
+						Point point1 = new Point(i, j);
+						CvInvoke.cvFloodFill(image_copy.Ptr, point1, new MCvScalar(255, 255, 255, 255),
+						new MCvScalar(0, 0, 0),
+						new MCvScalar(0, 0, 0), out comp,
+						Emgu.CV.CvEnum.CONNECTIVITY.EIGHT_CONNECTED,
+						Emgu.CV.CvEnum.FLOODFILL_FLAG.DEFAULT, mask.Ptr);
+						if (comp.area < 10000)
+						{
+							image = image_copy.Copy();
+						}
+					}
+				}
+			}
+		}
+
 		public static Image<Bgr, byte> FindBlobs(Image<Bgr, byte> source)
 		{
 			//source._EqualizeHist();
 			var edges = new Image<Bgr, byte>(source.Width, source.Height);
+			// source.SmoothMedian(5);
 			for (int i = 0; i < 3; i++)
-				edges[i] = source[i].Canny(new Gray(100), new Gray(150));
-			var grayEdges = edges.Convert<Gray, byte>();
-			var distTransformed = new Image<Gray, float>(source.Width, source.Height);
-			CvInvoke.cvDistTransform(grayEdges.Ptr, distTransformed.Ptr, DIST_TYPE.CV_DIST_L2, 3, new[] { 1f, 1f }, IntPtr.Zero);
-			var image = grayEdges; // SuperCoolEdgeMedianSmooth(source, MarkBlobs(distTransformed));
-            return image.Convert<Bgr, Byte>();
+				edges[i] = source[i].Canny(new Gray(100), new Gray(100));
+			var grayEdges = edges.Convert<Gray, byte>().Convert<Bgr, byte>();
+			// var distTransformed = new Image<Gray, float>(source.Width, source.Height);
+			// CvInvoke.cvDistTransform(grayEdges.Ptr, distTransformed.Ptr, DIST_TYPE.CV_DIST_L2, 3, new[] { 1f, 1f }, IntPtr.Zero);
+			// FloodFill(grayEdges);
+			TrackBlobs(grayEdges);
+			return grayEdges;
 		}
+
+		
 
 		private static Image<Bgr, byte> GaussEdgeDetector(Image<Bgr, byte> source)
 		{
